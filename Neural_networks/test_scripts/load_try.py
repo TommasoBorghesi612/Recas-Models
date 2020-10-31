c732bc6f5dd098ef60935a6668221bdcf38ce71d
Tommaso Borghesi <tborghes@ui03.recas.ba.infn.it>
2020-10-31 12:37:57 +0100

    fixed script for time testing on loading times

--- Neural_networks/test_scripts/load_try.py
+++ Neural_networks/test_scripts/load_try.py
@@ -14,65 +14,63 @@ print("Current Time =", current_time)
 
 temp = np.array(pd.read_hdf('/lustrehome/tborghes/compact_data/data_1.h5', key='data', stop=1000000), dtype = np.float32)
 
-print('loading 1 complete')
+print('1 Million data -->')
 
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 print("Current Time =", current_time)
 
-temp2 = np.array(pd.read_hdf('/lustrehome/tborghes/compact_data/data_1.h5', key='data', stop=1500000), dtype = np.float32)
+temp = np.array(pd.read_hdf('/lustrehome/tborghes/compact_data/data_1.h5', key='data', stop=1500000), dtype = np.float32)
 
-print('loading 2 complete')
-
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-print("Current Time =", current_time)
-
-temp3 = np.array(pd.read_hdf('/lustrehome/tborghes/compact_data/data_1.h5', key='data', stop=2000000), dtype = np.float32)
-
-print('loading 3 complete')
+print('1.5 Million data -->')
 
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 print("Current Time =", current_time)
 
-temp4 = np.array(pd.read_hdf('/lustrehome/tborghes/compact_data/data_1.h5', key='data', start = 3000000), dtype = np.float32)
+temp = np.array(pd.read_hdf('/lustrehome/tborghes/compact_data/data_1.h5', key='data', stop=2000000), dtype = np.float32)
 
-print('loading 4 complete')
-
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-print("Current Time =", current_time)
-
-con_small = tf.concat([temp, temp2], axis = 0)
-
-print('concat 1 complete')
+print('2 Million data -->')
 
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 print("Current Time =", current_time)
 
-con_small = tf.concat([temp, temp2], axis = 0)
+temp4 = np.array(pd.read_hdf('/lustrehome/tborghes/compact_data/data_1.h5', key='data', stop = 2250000), dtype = np.float32)
 
-print('concat 2 complete')
-
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-print("Current Time =", current_time)
-
-con_small = tf.concat([temp, temp2], axis = 0)
-
-print('concat 3 complete')
+print('2.25 Million data -->')
 
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 print("Current Time =", current_time)
 
-con_large = tf.concat([temp, temp2, temp3, temp4], axis = 0)
+temp = np.array(pd.read_hdf('/lustrehome/tborghes/compact_data/data_1.h5', key='data', stop = 2500000), dtype = np.float32)
 
-print('concat 4 complete')
+print('2.5 Million data --> ')
 
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 print("Current Time =", current_time)
+
+temp = np.array(pd.read_hdf('/lustrehome/tborghes/compact_data/data_1.h5', key='data', stop = 2750000), dtype = np.float32)
+
+print('2.75 Million data --> ')
+
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+print("Current Time =", current_time)
+
+temp = np.array(pd.read_hdf('/lustrehome/tborghes/compact_data/data_1.h5', key='data', stop = 3000000), dtype = np.float32)
+
+print('3 Million data --> ')
+
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+print("Current Time =", current_time)
+
+# con_small = tf.concat([temp, temp2], axis = 0)
+
+# print('concat 1 complete')
+
+
 
